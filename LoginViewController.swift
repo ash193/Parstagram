2606b68bbb0ccc320aa3cd334156770a8cc0cfd5
ash193 <52557802+ash193@users.noreply.github.com>
2021-10-11 20:41:25 -0400

    Updates

---
+++ LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Ashley R on 10/11/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+ 
+}
